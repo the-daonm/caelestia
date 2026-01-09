76c4cf8dacfef89041492f1bf6417879a95ca759
daonm <daobilionusd@gmail.com>
2026-01-09 09:13:47 +0700

    feat: change for personal use and add greetd and docker to install

--- fish/config.fish
+++ fish/config.fish
@@ -3,14 +3,17 @@ if status is-interactive
     starship init fish | source
 
     # Direnv + Zoxide
-    command -v direnv &> /dev/null && direnv hook fish | source
-    command -v zoxide &> /dev/null && zoxide init fish --cmd cd | source
+    command -v direnv &>/dev/null && direnv hook fish | source
+    command -v zoxide &>/dev/null && zoxide init fish --cmd cd | source
 
     # Better ls
     alias ls='eza --icons --group-directories-first -1'
 
     # Abbrs
-    abbr lg 'lazygit'
+    abbr cl 'clear'
+    abbr gsudo 'sudo -E'
+    abbr ld lazydocker
+    abbr lg lazygit
     abbr gd 'git diff'
     abbr ga 'git add .'
     abbr gc 'git commit -am'
@@ -27,13 +30,20 @@ if status is-interactive
     abbr gco 'git checkout'
     abbr gsh 'git show'
 
-    abbr l 'ls'
+    abbr l ls
     abbr ll 'ls -l'
     abbr la 'ls -a'
     abbr lla 'ls -la'
 
     # Custom colours
-    cat ~/.local/state/caelestia/sequences.txt 2> /dev/null
+    cat ~/.local/state/caelestia/sequences.txt 2>/dev/null
+
+    fzf --fish | source
+    zoxide init fish | source
+
+    set -gx MY_FZF_CMD "fd --type f --hidden --exclude '.steam*' --exclude 'steam'"
+    set -gx FZF_DEFAULT_COMMAND $MY_FZF_CMD
+    set -gx FZF_CTRL_T_COMMAND $MY_FZF_CMD
 
     # For jumping between prompts in foot terminal
     function mark_prompt_start --on-event fish_prompt

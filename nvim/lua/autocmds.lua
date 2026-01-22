2068f64320d445a22db9028e10dafa6fb0c63cd8
daonm <daobilionusd@gmail.com>
2026-01-22 13:10:11 +0700

    Add many

--- nvim/lua/autocmds.lua
+++
@@ -1,9 +0,0 @@
-require "nvchad.autocmds"
-
-vim.filetype.add({
-  pattern = {
-    -- Matches docker-compose.yml, docker-compose.yaml, compose.yml, compose.yaml
-    ["docker-compose.*%.ya?ml"] = "yaml.docker-compose",
-    ["compose.*%.ya?ml"] = "yaml.docker-compose",
-  },
-})
